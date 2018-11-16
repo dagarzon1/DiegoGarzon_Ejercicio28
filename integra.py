0425db9fa1170436f4b9d9563e001383ed0e501d
dagarzon1 <37335375+dagarzon1@users.noreply.github.com>
2018-11-16 00:18:29 -0500

    Update integra.py

--- integra.py
+++ integra.py
@@ -12,11 +12,12 @@ int_analitica = np.exp(1) - 1
 
 for i in k:
     d = np.loadtxt(i)
-    d = d - int_analitica
+    d = np.abs(d - int_analitica)
     d = d / int_analitica
     d = d * 100
-    dat.append(d[0])
-n = np.logspace(2,9,4)
+    dat.append(d)
+n = np.logspace(2,8,7)
 
-lt.hist(np.log(n),dat)
+plt.plot(np.log(n),dat)
 plt.savefig("integra.pdf")
+
