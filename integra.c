d5e2df22e144a2353cc99fb106ed1e4376606acf
dagarzon1 <37335375+dagarzon1@users.noreply.github.com>
2018-11-15 23:39:14 -0500

    Update integra.c

--- integra.c
+++ integra.c
@@ -9,18 +9,18 @@ using namespace std;
 double f(double x);
 
 int main(int argc, char * argv[]){
-    
+
     int N = atof(argv[1]);
     int rank, size;
-        
+
     MPI_Init (&argc, &argv);
     MPI_Comm_rank (MPI_COMM_WORLD, &rank);
     MPI_Comm_size (MPI_COMM_WORLD, &size);
-    
-    
+
+
     int n = N/size;
-    
-    if (rank != 0){
+    double s, total;
+
         srand(time(NULL));
         double mx = 1;
         double mn = 0;
@@ -36,29 +36,18 @@ int main(int argc, char * argv[]){
             summ = summ + puntos[i];
         }
         summ = summ / (double) n;
-        MPI_Send(&summ, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
-    }
-    
+        MPI_Reduce(&summ,&s, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
     if (rank == 0){
-        char filename[128];
-        FILE *out;
-        double S = 0;
-        for (int i=1;i<n;i++){
-            double s;
-            MPI_Recv(&s, 1, MPI_DOUBLE, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            S = S + s;
-        }
-        S = S / (double) N;
-        sprintf(filename, "integra_%d.txt", N);
-        fprintf(out, "%f\n", S);
-        fclose(out);
+        total = s / (double) size;
+        print(total);
     }
-    
+    MPI_Barrier(MPI_COMM_WORLD);
+    MPI_Finalize();
     return 0;
-    
+
 }
 
 double f(double x){
-    
+
     return exp(x);
 }
