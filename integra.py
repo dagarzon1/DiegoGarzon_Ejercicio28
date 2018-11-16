819a5792002a6795b9c4f4be1ee5940adfde7062
dagarzon1 <37335375+dagarzon1@users.noreply.github.com>
2018-11-15 23:47:29 -0500

    Create integra.py

---
+++ integra.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+import sys
+import glob
+
+k = glob.glob("*.txt")
+dat = []
+
+int_analitica = np.exp(1) - 1
+
+for i in k:
+    d = np.loadtxt(i)
+    d = d - int_analitica
+    d = d / int_analitica
+    d = d * 100
+    dat.append(d[0])
+n = np.logspace(2,9,4)
+
+lt.hist(np.log(n),dat)
+plt.savefig("integra.pdf")
